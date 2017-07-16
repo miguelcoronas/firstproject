3a61a51e1644fa47a5af5ac05a07d3cfd44e4ae0
Miguel <miguelcoronas@outlook.com>
2017-07-16 18:19:51 -0500

    feat(all): include scroll navigation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+/*  ---- scroll navigation */
+$(document).ready(function() {
+	$('a.navbar-brand, a.nav-link').click(function() {
+		var $secID = $(this);
+		$('html, body').stop().animate({
+			scrollTop: ($($secID.attr('href')).offset().top - 57)}, 1000);
+	});
+	$('body').scroll.spy ({
+		target: ".navbar-fixed-top", offset:70
+	})
+});
